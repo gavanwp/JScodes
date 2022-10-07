02aa861959e551ca76e626ceab77d78726bdaebc
gavanwp <wpgavan@gmail.com>
2022-10-07 21:17:21 +0500

    js class codes

---
+++ class-01/app.js
@@ -0,0 +1,22 @@
+var firstname = "Gavan ";
+var lastnamen= "kumar";
+
+
+console.log(firstname);
+console.log(lastnamen);
+var fullname =firstname+lastnamen;
+console.log(fullname)
+alert(fullname)
+
+var email = "gavankumar067@gmail.com";
+var mobilenumber = "4454534";
+var contact = emal + mobilenumber; 
+
+
+console.log(email)
+console.log(mobilenumber)
+console.log(contact+url)
+
+
+
+
