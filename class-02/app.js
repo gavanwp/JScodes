643449777842386fcd9195dadf45a1bcaac13bf8
gavanwp <wpgavan@gmail.com>
2022-10-09 14:52:19 +0500

    new code

--- class-02/app.js
+++ class-02/app.js
@@ -16,5 +16,40 @@ console.log (b) ;
   console.log (b) ;
 }
 const author = "wordpress.org";
+c = 400;
+console.log(c)
+
+
 // let author =" freelancer.com"
 // const admin 
+
+//Operators in JavaScript 
+//IN 3+ 4 '+' is the operator and 2,3 are operands
+//1. unary operator - It has single operand (x= -x)
+// Exmples of inary operator 
+
+d = 300 ; 
+
+d = -d ;
+console.log(d)
+
+//1. Binary  operator - It has single operand (x = +6)
+// Exmples of bnary operator 
+ c = 300+20;
+ console.log(c)
+
+ // Arithmetic operators in action in JS
+  var num1 = 40;
+  var num2 = 30;
+
+
+
+
+  console.log(" The value of  num1 + num2 is "  + ( num1 + num2 ) ); 
+  console.log(" The value of  num1 - num2 is "  + ( num1 - num2 ) ); 
+  console.log(" The value of  num1 * num2 is "  + ( num1 * num2 ) ); 
+  console.log(" The value of  num1 / num2 is "  + ( num1 / num2 ) ); 
+  console.log(" The value of  num1 ** num2 is "  + ( num1 ** num2 ) ); 
+  console.log(" The value of  num1 ++ is "  + ( num1 ++ ) ); 
+  console.log(" The value of  ++ num2 is "  + ( ++  num2 ) ); 
+
