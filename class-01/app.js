0e83e233a6712382afe5244e902e6ec7dea0d92d
gavanwp <wpgavan@gmail.com>
2022-10-09 15:47:05 +0500

    update

--- class-01/app.js
+++ class-01/app.js
@@ -18,13 +18,25 @@ var mobilenumber = "4454534";
 var contact = email + mobilenumber; 
 
 
-console.log(email)
-console.log(mobilenumber)
+console.log(email);
+console.log(mobilenumber);
  
-console.log(contact)
-alert(contact)
+console.log(contact);
+alert(contact);
 
 
+var fname =("gavan");
+var lname =("kumar");
+var bio =(" Is a wordpress developer and designer ");
+var fiverr=("fiverr.com");
+
+console.log(fname+lname+bio) ;
 
 
+var temp = `${fname} ${lname}  is a wordpress developer and designer working as a freelancer on ${fiverr}`;  
+console.log(temp);
+
+// var len = fname.length;
+//  console.log('length of fname is ${len');
+console.log("hello Javascript... \n How are you  ")
 
