dcdf0fd819dc51f75f328d2d8e0cd0614f058ce3
gavanwp <wpgavan@gmail.com>
2022-10-09 09:33:13 +0500

    class-02

---
+++ class-02/main.js
@@ -0,0 +1,20 @@
+console.log("JavaScript tutorial 3 var , let and const") ;
+
+// const author = "gavanwp"; author = 5 Throws an error because constant connot be cheanged 
+let c = "gavanwp ";
+var b = " This is a var " ;
+// var b = "JavaScript course "
+
+// let c = "JavaScript course for free "
+
+console.log (c) ;
+console.log (b) ;
+// console.log (b)
+// console.log (c)
+{
+  let b = "apnadigitaldokan " ;
+  console.log (b) ;
+}
+const author = "wordpress.org";
+// let author =" freelancer.com"
+// const admin 
