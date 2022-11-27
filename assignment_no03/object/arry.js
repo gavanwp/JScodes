5bfe48f50babe63c279f51dfe2ce88a77e8de3a3
gavanwp <wpgavan@gmail.com>
2022-11-27 21:24:45 +0500

    update

---
+++ assignment_no03/object/arry.js
@@ -0,0 +1,132 @@
+var UsersData = [
+  {
+    name: "Leanne Graham",
+    username: "Bret",
+    email: ["Sincere@april.biz"],
+    phone: [
+      {
+        number: "1-770-736-8031",
+      },
+      {
+        number: "010-692-6593",
+      },
+    ],
+  },
+  {
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: ["Shanna@melissa.tv", "Nathan@yesenia.net", "Sincere@april.biz"],
+    phone: [
+      {
+        number: "032-692-5868",
+      },
+    ],
+  },
+  {
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: ["Nathan@yesenia.net", "info@gmail.com"],
+    phone: [
+      {
+        number: "(254)954-1289",
+      },
+      {
+        number: "1-477-935-8478",
+      },
+      {
+        number: "76495-3109",
+      },
+    ],
+  },
+  {
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: ["Nathan@yesenia.net", "info@gmail.com"],
+    phone: [
+      {
+        number: "(254)954-1289",
+      },
+      {
+        number: "1-477-935-8478",
+      },
+      {
+        number: "76495-3109",
+      },
+    ],
+  },
+];
+
+for (let index = 0; index < UsersData.length; index++) {
+  var contaienr = document.getElementById("contaienr");
+  var box = document.createElement("div");
+  contaienr.appendChild(box);
+  box.setAttribute("id", "box");
+
+  //Name
+  var Name = document.createElement("h2");
+  box.appendChild(Name);
+  var idtext = document.createTextNode("Name :");
+  Name.appendChild(idtext);
+  Name.setAttribute("class", "Name");
+  var name1 = document.createElement("h2");
+  box.appendChild(name1);
+  var name1Text = document.createTextNode(UsersData[index].name);
+  name1.appendChild(name1Text);
+  name1.setAttribute("class", "Name");
+
+  ///  //  User Name
+  var username = document.createElement("h2");
+  box.appendChild(username);
+
+  var nameText = document.createTextNode("Username :");
+  username.appendChild(nameText);
+  username.setAttribute("class", "Name");
+  var UserName = document.createElement("h2");
+  box.appendChild(UserName);
+  var UserNameText = document.createTextNode(UsersData[index].username);
+  UserName.appendChild(UserNameText);
+  UserName.setAttribute("class", "Name");
+
+  // Email Address
+  var email = document.createElement("h2");
+  box.appendChild(email);
+  var emailText = document.createTextNode("Email :");
+  email.appendChild(emailText);
+  email.setAttribute("class", "Name");
+  // div
+  var div = document.createElement("div");
+  box.appendChild(div);
+  div.setAttribute("id", "div");
+
+  for (let mail = 0; mail < UsersData[index].email.length; mail++) {
+    // sub div
+    var subdiv = document.createElement("div");
+    div.appendChild(subdiv);
+    subdiv.setAttribute("class", "subdiv");
+
+    var Email = document.createElement("h2");
+    subdiv.appendChild(Email);
+    var EmailText = document.createTextNode(UsersData[index].email[mail]);
+    Email.appendChild(EmailText);
+    Email.setAttribute("class", "Name");
+  }
+  // Mobile Number
+  var phone = document.createElement("h2");
+  box.appendChild(phone);
+  var PhoneText = document.createTextNode("Phone Number:");
+  phone.appendChild(PhoneText);
+  phone.setAttribute("class", "Name");
+  var div2 = document.createElement("div");
+  box.appendChild(div2);
+  div2.setAttribute("id", "div2");
+  for (let i = 0; i < UsersData[index].phone.length; i++) {
+    var subdiv2 = document.createElement("div");
+    div2.appendChild(subdiv2);
+    subdiv2.setAttribute("class", "subdiv");
+    var phonE = document.createElement("h2");
+    subdiv2.appendChild(phonE);
+    var PhonEText = document.createTextNode(UsersData[index].phone[i].number);
+    phonE.appendChild(PhonEText);
+    phonE.setAttribute("class", "Name");
+  }
+}
---
+++ assignment_no03/object/object.js
@@ -0,0 +1,380 @@
+var add = [
+  {
+    id: 200,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+ 
+];
+
+for (let index = 0; index < add.length; index++) {
+  var container = document.getElementById("container");
+  var box = document.createElement("div");
+  container.appendChild(box);
+  box.setAttribute("id", "box");
+  // ID
+  var id = document.createElement("h2");
+  box.appendChild(id);
+  var idtext = document.createTextNode("ID" + " " + add[index].id);
+  id.appendChild(idtext);
+  id.setAttribute("id", "textid");
+
+  //  name
+  var Name = document.createElement("h2");
+  box.appendChild(Name);
+  var nameText = document.createTextNode("Name :" + " " + add[index].name);
+  Name.appendChild(nameText);
+  Name.setAttribute("class", "nametext");
+
+  // username
+
+  var username = document.createElement("h2");
+  box.appendChild(username);
+  var usernameText = document.createTextNode(
+    "Username :" + " " + add[index].username
+  );
+  username.appendChild(usernameText);
+  username.setAttribute("class", "nametext");
+
+  // Email
+  var email = document.createElement("h2");
+  box.appendChild(email);
+  var emailText = document.createTextNode("Email :" + " " + add[index].email);
+  email.appendChild(emailText);
+  email.setAttribute("class", "nametext");
+
+  // Address
+  var address = document.createElement("h2");
+  box.appendChild(address);
+  var addressText = document.createTextNode("Address :-");
+  address.appendChild(addressText);
+  address.setAttribute("class", "nametext");
+
+  //street
+  var street = document.createElement("h2");
+  address.appendChild(street);
+  var streetText = document.createTextNode(
+    "Street :" + " " + add[index].address.street
+  );
+  street.appendChild(streetText);
+  street.setAttribute("class", "nametext1");
+
+  // suite
+  var suite = document.createElement("h2");
+  address.appendChild(suite);
+  var suiteText = document.createTextNode(
+    "Suite :" + " " + add[index].address.suite
+  );
+  suite.appendChild(suiteText);
+  suite.setAttribute("class", "nametext1");
+
+  // city
+  var city = document.createElement("h2");
+  address.appendChild(city);
+  var cityText = document.createTextNode(
+    "City :" + " " + add[index].address.city
+  );
+  city.appendChild(cityText);
+  city.setAttribute("class", "nametext1");
+
+  // zipcode
+  var zipcode = document.createElement("h2");
+  address.appendChild(zipcode);
+  var zipcodeText = document.createTextNode(
+    "Zipcode :" + " " + add[index].address.zipcode
+  );
+  zipcode.appendChild(zipcodeText);
+  zipcode.setAttribute("class", "nametext1");
+
+  // GEO
+  var geo = document.createElement("h2");
+  address.appendChild(geo);
+  var geoText = document.createTextNode("GEO :-");
+  geo.appendChild(geoText);
+  geo.setAttribute("class", "nametext");
+
+  var lat = document.createElement("h2");
+  geo.appendChild(lat);
+  var latText = document.createTextNode(
+    "lat :" + " " + add[index].address.geo.lat
+  );
+  lat.appendChild(latText);
+  lat.setAttribute("class", "nametext1");
+
+  var lng = document.createElement("h2");
+  geo.appendChild(lng);
+  var lngText = document.createTextNode(
+    "lng :" + " " + add[index].address.geo.lng
+  );
+  lng.appendChild(lngText);
+  lng.setAttribute("class", "nametext1");
+
+  //phone
+  var phone = document.createElement("h2");
+  box.appendChild(phone);
+  var phoneText = document.createTextNode("phone :" + " " + add[index].phone);
+  phone.appendChild(phoneText);
+  phone.setAttribute("class", "nametext");
+
+  // website
+  var website = document.createElement("h2");
+  box.appendChild(website);
+  var websiteText = document.createTextNode(
+    "website :" + " " + add[index].website
+  );
+  website.appendChild(websiteText);
+  website.setAttribute("class", "nametext");
+
+  // company
+  var company = document.createElement("h2");
+  box.appendChild(company);
+  var companyText = document.createTextNode("Company :-");
+  company.appendChild(companyText);
+  company.setAttribute("class", "nametext");
+
+  var companyname = document.createElement("h2");
+  company.appendChild(companyname);
+  var companynameText = document.createTextNode(
+    "Name :" + " " + add[index].company.name
+  );
+  companyname.appendChild(companynameText);
+  companyname.setAttribute("class", "nametext1");
+  var catchPhrase = document.createElement("h2");
+  company.appendChild(catchPhrase);
+  var catchPhraseTex = document.createTextNode(
+    "CatchPhrase : " + " " + add[index].company.catchPhrase
+  );
+  catchPhrase.appendChild(catchPhraseTex);
+  catchPhrase.setAttribute("class", "nametext1");
+
+  var bs = document.createElement("h2");
+  company.appendChild(bs);
+  var bsText = document.createTextNode("Bs :" + " " + add[index].company.bs);
+  bs.appendChild(bsText);
+  bs.setAttribute("class", "nametext1");
+}
